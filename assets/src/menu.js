83aa6fac25cab34dd3ea18021b2e649168d87f90
emilianoar23 <emilianoar23@gmail.com>
2023-09-20 11:35:11 -0300

    cambio de menu v2

--- assets/src/menu.js
+++ assets/src/menu.js
@@ -1,36 +1,4 @@
-//scroll home 
-$('.logo').click(function (event) {
-  let target = $('.hero').offset().top;
-
-  $('html, body').animate({
-    scrollTop: target
-  }, 900);
-  event.preventDefault();
-});
-
-//scroll nav
-$(window).scroll(function () {
-  if ($(window).scrollTop() > 100) {
-    $('#header-js').addClass('sticky');
-  } else {
-    $('#header-js').removeClass('sticky');
-  }
-});
-
-$('.mobile-toggle').click(function () {
-  if ($('#header-js').hasClass('open-nav')) {
-    $('#header-js').removeClass('open-nav');
-  } else {
-    $('#header-js').addClass('open-nav');
-  }
-});
-
-$('nav a').click(function (event) {
-  let id = $(this).attr("href");
-  let offset = 70;
-  let target = $(id).offset().top - offset;
-  $('html, body').animate({
-    scrollTop: target
-  }, 500);
-  event.preventDefault();
-});
+function toggleNav() {
+  const navLinks = document.querySelector('.nav-links');
+  navLinks.style.display = navLinks.style.display === 'none' || navLinks.style.display === '' ? 'flex' : 'none';
+}
