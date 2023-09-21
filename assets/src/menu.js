abb8c9e91dd35b40b5c7e2a72e21dcf829f43a14
emilianoar23 <emilianoar23@gmail.com>
2023-09-21 10:13:40 -0300

    cambio de menu responsive

--- assets/src/menu.js
+++ assets/src/menu.js
@@ -1,4 +1,27 @@
-function toggleNav() {
-  const navLinks = document.querySelector('.nav-links');
-  navLinks.classList.toggle('active');
-}
+const menuSlide = () => {
+  const menuIcon = document.querySelector(".menu-icon");
+  const navLinks = document.querySelector(".nav-links");
+  const navLinksInner = document.querySelectorAll(".nav-links li");
+
+  //menu-icon click event
+  menuIcon.addEventListener("click", () => {
+    //toggle active class
+    navLinks.classList.toggle("menu-active");
+
+    //animate navLinks
+    navLinksInner.forEach((li, index) => {
+      if (li.style.animation) {
+        li.style.animation = "";
+      } else {
+        li.style.animation = `navLinkAnime 0.5s ease forwards ${
+          index / 7 + 0.3
+        }s`;
+      }
+    });
+
+    //toggle for menu-icon animation
+    menuIcon.classList.toggle("span-anime");
+  });
+};
+
+menuSlide();
